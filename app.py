2d3d151e0cce7d176aeec423d5f635765f15ec6e
游家和 <chiahe@myMacBookAir.local>
2023-07-31 11:14:15 +0800

    message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 from urllib.request import urlretrieve
 
 # ======python的函數庫==========
