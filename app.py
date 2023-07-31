41eeef58cd0919950ace4206b10090190230f66b
游家和 <chiahe@myMacBookAir.local>
2023-07-31 15:32:06 +0800

    message

--- app.py
+++ app.py
@@ -20,41 +20,77 @@ import requests
 
 app = Flask(__name__)
 
-configuration = Configuration(access_token='CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('CHANNEL_SECRET')
+configuration = Configuration(
+    access_token='uM8W7WzujbTNQuVhsRD2veLUscpkeT2Tx0xtRAXxRKRHcUrvxShxrRDrkcCVaM0tKzemGTrch8vCnjjv/XbPxzbSMcrB6uz7QNsfb2a1HeGrPYZZnMyLq24rDc4Tp08eXpY+IKKxgBFmNlc6YCfYrAdB04t89/1O/w1cDnyilFU=')  # CHANNEL_ACCESS_TOKEN
+handler = WebhookHandler('d7e644e24ecda456d7ef5c933a7d544c')  # CHANNEL_SECRET
 
 
-@app.route("/", methods=['GET'])
-def hello():
-    return "Hello World!"
-
-
-@app.route("/", methods=['POST'])
+@app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+
     # get request body as text
     body = request.get_data(as_text=True)
-    print("Request body: " + body, "Signature: " + signature)
+    app.logger.info("Request body: " + body)
+
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        app.logger.info(
+            "Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
+
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    msg = event.message.text
-    print(msg)
-    msg = msg.encode('utf-8')
-    line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text=event.message.text))
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=10000)
+    app.run()
+
+# @app.route("/", methods=['GET'])
+# def hello():
+#     return "Hello World!"
+
+
+# @app.route("/", methods=['POST'])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     print("Request body: " + body, "Signature: " + signature)
+#     # handle webhook body
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         abort(400)
+#     return 'OK'
+
+
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     msg = event.message.text
+#     print(msg)
+#     msg = msg.encode('utf-8')
+#     line_bot_api.reply_message(
+#         event.reply_token, TextSendMessage(text=event.message.text))
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True, port=10000)
 
 '''
 ---------------------範例程式-----------------------
